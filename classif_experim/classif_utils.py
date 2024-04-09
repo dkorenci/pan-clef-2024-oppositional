01bed06decd5e1fe45bd5f07dac106cedb25a66d
damir <damir.korencic@gmail.com>
2024-04-09 16:33:38 +0200

    classif_utils.py - f1_score_negative_class asserts corrected

--- classif_experim/classif_utils.py
+++ classif_experim/classif_utils.py
@@ -7,8 +7,8 @@ f1_binary = partial(f1_score, average='binary')
 
 def f1_score_negative_class(y_true, y_pred):
     """ Calculate the F1 score for the negative class in a binary classification setting. """
-    assert set(y_true) == {0, 1}
-    assert set(y_pred) == {0, 1}
+    assert set(y_true).issubset({0, 1})
+    assert set(y_pred).issubset({0, 1})
     # Inverting the labels: 0 becomes 1 and 1 becomes 0
     y_true_inverted = [1 if label == 0 else 0 for label in y_true]
     y_pred_inverted = [1 if label == 0 else 0 for label in y_pred]
