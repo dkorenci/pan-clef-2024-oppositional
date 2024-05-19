99913c5327ad71a420998fa2bf04b776ad7afd9b
ndiez <nachodl.queso@gmail.com>
2024-05-20 00:21:56 +0200

    dataset_processers.py created

---
+++ data_tools/dataset_processers.py
@@ -0,0 +1,8 @@
+"""
+All functions that are used to process the dataset.
+
+They all should accept this arguments:
+    Tuple[pd.Series, pd.Series, pd.Series]: Texts, binary classes (1 - positive, 0 - negative), and text ids as pandas Series.
+
+And return the same type of data.
+"""
